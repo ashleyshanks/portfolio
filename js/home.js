07c563cf87b1bee143f0393429b7656533d7b149
undercoverfinch <ashleynshanks@gmail.com>
2026-04-29 15:50:53 -0700

    projects hero mobile mq

--- js/home.js
+++ js/home.js
@@ -19,23 +19,23 @@ window.addEventListener("scroll", handleSideScrollFwd);
 window.addEventListener("resize", handleSideScrollFwd);
 handleSideScrollFwd();
 
-const sideBkwdSection = document.querySelector(".marquee.bkwd");
-const bkwdText = sideBkwdSection.querySelector("p");
+// const sideBkwdSection = document.querySelector(".marquee.bkwd");
+// const bkwdText = sideBkwdSection.querySelector("p");
 
-function handleSideScrollBkwd() {
-  const rect = sideBkwdSection.getBoundingClientRect();
-  const vh = window.innerHeight;
+// function handleSideScrollBkwd() {
+//   const rect = sideBkwdSection.getBoundingClientRect();
+//   const vh = window.innerHeight;
 
-  // progress: when section enters → when it leaves
-  let progress = (vh - rect.top) / (vh + rect.height);
-  progress = Math.max(0, Math.min(1, progress));
+//   // progress: when section enters → when it leaves
+//   let progress = (vh - rect.top) / (vh + rect.height);
+//   progress = Math.max(0, Math.min(1, progress));
 
-  // move from right → left across screen
-  const x = -60 + progress * 80; // percentage
+//   // move from right → left across screen
+//   const x = -60 + progress * 80; // percentage
 
-  bkwdText.style.transform = `translateX(${x}%)`;
-}
+//   bkwdText.style.transform = `translateX(${x}%)`;
+// }
 
-window.addEventListener("scroll", handleSideScrollBkwd);
-window.addEventListener("resize", handleSideScrollBkwd);
-handleSideScrollBkwd();
+// window.addEventListener("scroll", handleSideScrollBkwd);
+// window.addEventListener("resize", handleSideScrollBkwd);
+// handleSideScrollBkwd();
